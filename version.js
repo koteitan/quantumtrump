79931bda778e108c8b3923e6f55630ccc19bf016
koteitan <ukaibutton@mac.com>
2017-10-09 01:55:16 +0900

    version 1.0 / move knownbug into version.js

--- version.js
+++ version.js
@@ -1,9 +1,10 @@
-document.getElementById("versiondisplay").innerHTML = "0.1";
+document.getElementById("versiondisplay").innerHTML = "1.0";
 document.getElementById("changelog").innerHTML = 
 "<ul>"+
 "<li>2017/10/08 23:51 実装開始。</li>"+
+"<li>2017/10/09 01:51 一旦実装完了。</li>"+
 "</ul>";
 document.getElementById("knownbugs").innerHTML = 
 "<ul>"+
-"<li>まだ動きません</li>"+
+"<li>iPhone, Androidではたぶん動きません。PCのブラウザでやってください</li>"+
 "</ul>";
