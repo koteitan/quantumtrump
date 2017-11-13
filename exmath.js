899a5a2bd77e826c84ec7de0ba7482b81c8dd19b
koteitan <ukaibutton@mac.com>
2017-11-13 23:06:21 +0900

    factorial(), perm2hash(), hash2perm() are added / add test codes of them

--- exmath.js
+++ exmath.js
@@ -13,6 +13,37 @@ var sqrt1p2=function(){
 var chisqr_1_0p05 = function(){
   -3.841
 }
+var factorial=function(n){
+  return [1,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,479001600,6227020800,87178291200,1307674368000,20922789888000,355687428096000,6402373705728000,121645100408832000,2432902008176640000][n];
+}
+var perm2hash=function(a0){
+  var a = a0.clone();
+  var base = a.length;
+  var h = 0;
+  for(var i=0;i<a.length;i++){
+    h += a[i]*factorial(--base);
+    for(var j=i+1;j<a.length;j++){
+      if(a[j]>a[i])a[j]--;
+    }
+  }
+  return h;
+};
+var hash2perm=function(h0,n0){
+  var h=h0;
+  var n=n0-1;
+  var a=new Array(n0);
+  for (i=0;i<n0;i++){
+    a[i] = Math.floor(h/factorial(n));
+    h   -= factorial(n)*a[i];
+    n--;
+  }
+  for(i=n0-2;i>=0;i--){
+    for (j=i+1;j<n0;j++){
+      if(a[i]<=a[j])a[j]++;
+    }
+  }
+  return a;
+};
 //xorshift random object
 var XorShift=function(s){
   if(s==undefined) s=(new Date()).getTime();
