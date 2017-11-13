c726be695df65e25927636222a8eb2cb9a73ec80
koteitan <ukaibutton@mac.com>
2017-11-14 00:27:58 +0900

    version 2.0.

--- version.js
+++ version.js
@@ -1,8 +1,9 @@
-document.getElementById("versiondisplay").innerHTML = "1.0";
+document.getElementById("versiondisplay").innerHTML = "2.0";
 document.getElementById("changelog").innerHTML = 
 "<ul>"+
 "<li>2017/10/08 23:51 実装開始。</li>"+
 "<li>2017/10/09 01:51 一旦実装完了。</li>"+
+"<li>2017/11/14 00:15 ２値表示だった確率をグレースケールにした。(その代わり計算量の関係でカードが６枚になった…)</li>"+
 "</ul>";
 document.getElementById("knownbugs").innerHTML = 
 "<ul>"+
